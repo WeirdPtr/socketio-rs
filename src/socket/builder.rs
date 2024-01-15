bd03f9d074ca99b7d2b155488036a46149ff0fc9
WeirdPtr <weirdptr@weirdptr.com>
2024-01-15 19:11:04 +0100

    feat(socket): rework inner networking

--- src/socket/builder.rs
+++ src/socket/builder.rs
@@ -1,39 +1,87 @@
 use super::{Socket, SocketReadStream, SocketWriteSink};
 use crate::{
     enums::{connection::ConnectionType, protocol::ProtocolVersion},
+    get_empty_body,
     parser::Packet,
+    util::crate_user_agent,
+    Request,
 };
+use bytes::Bytes;
+use fastwebsockets::handshake;
+use futures_util::future::BoxFuture;
 use futures_util::lock::Mutex;
-use futures_util::{future::BoxFuture, StreamExt};
+use http_body_util::Empty;
 use std::{collections::HashMap, sync::Arc};
-use tokio_tungstenite::tungstenite::{
-    client::IntoClientRequest, http::Request, protocol::WebSocketConfig,
-};
+use tokio::net::TcpStream;
+use url::Url;
 
-pub type SocketListenerFn = dyn Fn(Packet, Arc<Mutex<SocketReadStream>>, Arc<Mutex<SocketWriteSink>>) -> BoxFuture<'static, ()>
+#[cfg(feature = "proxy")]
+use crate::util::proxy::TcpProxy;
+
+pub type SocketListenerFn = dyn Fn(
+        Arc<Packet>,
+        Arc<Mutex<SocketReadStream>>,
+        Arc<Mutex<SocketWriteSink>>,
+    ) -> BoxFuture<'static, ()>
     + Send
     + Sync
     + 'static;
 
+struct SpawnExecutor;
+impl<Fut> hyper::rt::Executor<Fut> for SpawnExecutor
+where
+    Fut: std::future::Future + Send + 'static,
+    Fut::Output: Send + 'static,
+{
+    fn execute(&self, fut: Fut) {
+        tokio::task::spawn(fut);
+    }
+}
+
 pub struct SocketBuilder {
     protocol: ProtocolVersion,
-    request: Request<()>,
+    request: Request<Empty<Bytes>>,
     connection_type: ConnectionType,
     namespace: Option<String>,
     query: HashMap<String, String>,
     listeners: HashMap<String, Vec<Box<SocketListenerFn>>>,
     wildcard_listener: Option<Arc<Box<SocketListenerFn>>>,
+    force_handshake: bool,
     #[cfg(feature = "proxy")]
     proxy: Option<String>,
-    ping_interval: Option<u32>,
+    #[cfg(feature = "proxy")]
+    ignore_invalid_proxy: bool,
+    #[cfg(feature = "proxy")]
+    ignore_proxy_env_vars: bool,
+}
+
+#[cfg(feature = "proxy")]
+impl SocketBuilder {
+    pub fn proxy<P>(mut self, proxy: P) -> Self
+    where
+        P: Into<String>,
+    {
+        self.proxy = Some(proxy.into());
+        self
+    }
+
+    pub fn ignore_invalid_proxy(&mut self, ignore: bool) -> &mut Self {
+        self.ignore_invalid_proxy = ignore;
+        self
+    }
+
+    pub fn ignore_proxy_env_vars(&mut self, ignore: bool) -> &mut Self {
+        self.ignore_proxy_env_vars = ignore;
+        self
+    }
 }
 
 impl SocketBuilder {
-    pub fn new<R>(request: R) -> Self
+    pub fn new_with_request<R>(request: R) -> Self
     where
-        R: IntoClientRequest + Unpin,
+        R: Into<Request<Empty<Bytes>>> + Unpin,
     {
-        let request = request.into_client_request().unwrap();
+        let request = request.into();
 
         let request_uri = request.uri().to_string();
 
@@ -43,16 +91,37 @@ impl SocketBuilder {
             connection_type: ConnectionType::default(),
             namespace: None,
             wildcard_listener: None,
+            force_handshake: false,
             query: HashMap::new(),
             listeners: HashMap::new(),
             #[cfg(feature = "proxy")]
             proxy: None,
-            ping_interval: None,
+            #[cfg(feature = "proxy")]
+            ignore_invalid_proxy: false,
+            #[cfg(feature = "proxy")]
+            ignore_proxy_env_vars: false,
         };
 
         instance.transform_raw_query(request_uri);
 
         instance
+    }
+
+    pub fn new<'a>(uri: impl Into<&'a str>) -> Self {
+        let url = Url::parse(uri.into()).expect("Invalid url provided");
+
+        let request = hyper::Request::builder()
+            .uri(url.as_str())
+            .header("Host", url.host_str().unwrap_or(url.as_str()))
+            .header("Upgrade", "websocket")
+            .header("Connection", "upgrade")
+            .header("Sec-WebSocket-Version", "13")
+            .header("Sec-WebSocket-Key", handshake::generate_key())
+            .header("User-Agent", crate_user_agent())
+            .body(get_empty_body())
+            .expect("Socket request builder failed");
+
+        Self::new_with_request(request)
     }
 
     fn transform_raw_query<Q>(&mut self, query: Q)
@@ -62,11 +131,21 @@ impl SocketBuilder {
         let query: String = query.into();
 
         for query_entry in query.trim_start_matches('?').split('&') {
-            let query_entry = query_entry.split('=').collect::<Vec<&str>>();
-            if query_entry.len() == 2 {
-                self.query
-                    .insert(query_entry[0].to_string(), query_entry[1].to_string());
+            let entry = query_entry.split_once('=');
+
+            if entry.is_none() {
+                continue;
             }
+
+            let (mut key, value) = entry.unwrap();
+
+            let query_params_start = key.find('?');
+
+            if query_params_start.is_some() {
+                key = key.split_at(query_params_start.unwrap()).1[1..].as_ref();
+            }
+
+            self.query.insert(key.to_owned(), value.to_owned());
         }
 
         let engine_version = self.query.get("EIO");
@@ -80,30 +159,6 @@ impl SocketBuilder {
         if engine_version.is_ok() {
             self.protocol(ProtocolVersion::from(engine_version.unwrap()));
         }
-    }
-
-    pub fn build_url(&self) -> String {
-        let mut url = String::new();
-
-        if let Some(namespace) = &self.namespace {
-            if !url.ends_with('/') {
-                url.push('/');
-            }
-            url.push_str(namespace);
-        }
-
-        if !self.query.is_empty() {
-            url.push('?');
-            for (key, value) in &self.query {
-                url.push_str(key);
-                url.push('=');
-                url.push_str(value);
-                url.push('&');
-            }
-            url.pop();
-        }
-
-        url
     }
 
     fn listener_boxed<'e, E>(&mut self, event: E, listener: Box<SocketListenerFn>) -> &mut Self
@@ -146,15 +201,6 @@ impl SocketBuilder {
         self
     }
 
-    #[cfg(feature = "proxy")]
-    pub fn proxy<P>(mut self, proxy: P) -> Self
-    where
-        P: Into<String>,
-    {
-        self.proxy = Some(proxy.into());
-        self
-    }
-
     pub fn listeners(
         &mut self,
         listeners: HashMap<String, Vec<Box<SocketListenerFn>>>,
@@ -172,11 +218,16 @@ impl SocketBuilder {
         self
     }
 
+    pub fn force_handshake(&mut self, force: bool) -> &mut Self {
+        self.force_handshake = force;
+        self
+    }
+
     pub fn on<'e, E, L>(&mut self, event: E, listener: L) -> &mut Self
     where
         E: Into<&'e str>,
         L: for<'a> Fn(
-                Packet,
+                Arc<Packet>,
                 Arc<Mutex<SocketReadStream>>,
                 Arc<Mutex<SocketWriteSink>>,
             ) -> BoxFuture<'static, ()>
@@ -191,7 +242,7 @@ impl SocketBuilder {
     pub fn on_any<L>(&mut self, listener: L) -> &mut Self
     where
         L: for<'a> Fn(
-                Packet,
+                Arc<Packet>,
                 Arc<Mutex<SocketReadStream>>,
                 Arc<Mutex<SocketWriteSink>>,
             ) -> BoxFuture<'static, ()>
@@ -203,77 +254,87 @@ impl SocketBuilder {
         self
     }
 
-    pub fn ping_interval(&mut self, ping_interval: u32) -> &mut Self {
-        self.ping_interval = Some(ping_interval);
-        self
-    }
-
-    #[cfg(not(feature = "proxy"))]
-    pub async fn connect(self) -> Result<Socket, Box<dyn std::error::Error>> {
-        use tokio_tungstenite::connect_async_with_config;
-
-        let config = WebSocketConfig {
-            max_message_size: Some(usize::MAX),
-            max_frame_size: Some(usize::MAX),
-            ..Default::default()
-        };
-
-        let (ws_stream, _) = connect_async_with_config(self.request, Some(config), false).await?;
-
-        let (write, read) = ws_stream.split();
-
-        let socket = Socket::new(
-            read,
-            write,
-            Some(Arc::new(Mutex::new(self.listeners))),
-            self.wildcard_listener,
-            self.ping_interval,
-        );
-
-        Ok(socket)
-    }
-
     #[cfg(feature = "proxy")]
-    pub async fn connect(self) -> Result<Socket, Box<dyn std::error::Error>> {
-        use crate::util::proxy::inner::InnerProxy;
-        use tokio::net::TcpStream;
-        use tokio_tungstenite::client_async_with_config;
-
-        let config = WebSocketConfig {
-            max_message_size: Some(usize::MAX),
-            max_frame_size: Some(usize::MAX),
-            ..Default::default()
-        };
-
-        let proxy = self.proxy.clone().unwrap_or(
+    async fn establish_tunnel<'a>(&self, uri: &'a str) -> Result<TcpStream, std::io::Error> {
+        let proxy_str = self.proxy.clone().unwrap_or(
             std::env::var("HTTP_PROXY")
                 .unwrap_or(std::env::var("HTTPS_PROXY").unwrap_or(String::new()))
                 .to_owned(),
         );
 
-        if proxy.is_empty() {
-            return Err("No proxy provided".into());
+        if proxy_str.is_empty() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "No proxy provided",
+            ));
         }
 
-        let proxy = InnerProxy::from_proxy_str(proxy.as_str())?;
+        println!("Proxy: {}", proxy_str);
+        println!("URI: {}", uri);
 
-        let proxy_stream: TcpStream = proxy
-            .connect_async(self.request.uri().to_string().as_str())
+        let stream = TcpProxy::from_proxy_str(proxy_str.as_str())?
+            .connect(uri)
+            .await;
+
+        match stream {
+            Ok(stream) => return Ok(stream.into()),
+            Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, e)),
+        }
+    }
+
+    #[inline]
+    fn get_tcp_connect_str<'a, R>(request: R) -> String
+    where
+        R: Into<&'a Request<Empty<Bytes>>> + Unpin,
+    {
+        let request = request.into();
+        let uri = request.uri();
+
+        format!(
+            "{}:{}",
+            uri.host().expect("No host supplied"),
+            uri.port_u16().unwrap_or(80)
+        )
+    }
+
+    pub async fn connect(self) -> Result<Socket, Box<dyn std::error::Error>> {
+        #[cfg(feature = "proxy")]
+        let stream = self
+            .establish_tunnel(self.request.uri().to_string().as_str())
+            .await;
+
+        #[cfg(feature = "proxy")]
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                if self.ignore_invalid_proxy {
+                    TcpStream::connect(Self::get_tcp_connect_str(&self.request).as_str()).await?
+                } else {
+                    return Err(Box::new(e));
+                }
+            }
+        };
+
+        #[cfg(not(feature = "proxy"))]
+        let stream = TcpStream::connect(Self::get_tcp_connect_str(&self.request).as_str()).await?;
+
+        let ws = fastwebsockets::handshake::client(&SpawnExecutor, self.request, stream)
             .await?
-            .into();
+            .0;
 
-        let (write, read) = client_async_with_config(self.request, proxy_stream, Some(config))
-            .await?
-            .0
-            .split();
+        let (read, write) = ws.split(|s| tokio::io::split(s));
 
-        let socket = Socket::new(
+        let mut socket = Socket::new(
             read,
             write,
             Some(Arc::new(Mutex::new(self.listeners))),
             self.wildcard_listener,
-            self.ping_interval,
         );
+
+        if self.force_handshake {
+            socket.handshake().await?;
+            socket.start_ping_worker().await;
+        }
 
         Ok(socket)
     }
