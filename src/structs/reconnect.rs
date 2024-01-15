ab6cf9e3b84b84c34f4eee7600808ff85dafcbcc
WeirdPtr <weirdptr@weirdptr.com>
2024-01-15 20:50:35 +0100

    feat(structs): add reconnect config

---
+++ src/structs/reconnect.rs
@@ -0,0 +1,14 @@
+#[derive(Clone)]
+pub struct ReconnectConfiguration {
+    pub enable_reconnect: bool,
+    pub reconnect_count: Option<u8>,
+    pub reconnect_delay: u64,
+    pub request: hyper::Request<http_body_util::Empty<bytes::Bytes>>,
+    pub force_handshake: bool,
+    #[cfg(feature = "proxy")]
+    pub ignore_invalid_proxy: bool,
+    #[cfg(feature = "proxy")]
+    pub ignore_proxy_env_vars: bool,
+    #[cfg(feature = "proxy")]
+    pub proxy: Option<String>,
+}
