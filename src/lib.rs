a1975f40df5d3e01c37195f6e5290aec992c3ac9
WeirdPtr <weirdptr@weirdptr.com>
2024-01-16 19:45:20 +0100

    feat(socket): export empty `hyper::Response` (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub mod util;
 /// Re-export of `hyper::Request`
 pub type Request<T> = hyper::Request<T>;
 
+/// Re-export of `hyper::Body`
+pub type EmptyRequest = Request<http_body_util::Empty<bytes::Bytes>>;
+
 /// Re-export
 pub fn get_empty_body() -> http_body_util::Empty<bytes::Bytes> {
     http_body_util::Empty::new()
