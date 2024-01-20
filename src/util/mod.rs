1208ff8efa24f5b5fc465b016d57407ee36be67c
WeirdPtr <weirdptr@weirdptr.com>
2024-01-20 17:14:54 +0100

    feat(util): add tokio task helper

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,3 +6,11 @@ pub mod proxy;
 pub fn crate_user_agent() -> String {
     format!("socketio-rs v{}", env!("CARGO_PKG_VERSION"))
 }
+
+pub(crate) fn safe_spawn<F, T>(future: F)
+where
+    F: futures_util::Future<Output = T> + Send + 'static,
+    T: Send + 'static,
+{
+    drop(tokio::spawn(future));
+}
